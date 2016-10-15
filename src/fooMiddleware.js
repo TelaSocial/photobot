6d50cd1bedac0d9be07a97d58ba4a43586c3bd4c
Fabricio C Zuardi <fabricio@fabricio.org>
2016-10-15 17:33:18 -0300

    middleware that replies every document or photo message with the link

--- src/fooMiddleware.js
+++ src/fooMiddleware.js
@@ -1,6 +1,34 @@
 const fooMiddleware = (ctx, next) => next().then(() => {
-    console.log(JSON.stringify(ctx, ' ', 2));
-    ctx.telegram.sendMessage(ctx.update.message.chat.id, 'oi');
+    // console.log('\n\n\n===\n');
+    // console.log(JSON.stringify(ctx, ' ', 2));
+    // console.log('\n===\n');
+    const { telegram } = ctx;
+    const { message } = ctx.update;
+    const { document, photo } = message;
+    const docId = document ? document.file_id : null;
+    const photoId = Array.isArray(photo)
+        ? photo[photo.length - 1].file_id
+        : null;
+    // console.log('docId, photoId', docId, photoId);
+    const fileId = docId || photoId;
+    if (fileId) {
+        telegram.getFileLink(fileId).then(fileLink => {
+            ctx.telegram.sendMessage(
+                ctx.update.message.chat.id,
+                `fileURL: ${fileLink}`
+            );
+        });
+    }
+    // if (ctx.update.message.text === 'foo') {
+    //     ctx.telegram.sendMessage(ctx.update.message.chat.id, 'oi', {
+    //         reply_markup: {
+    //             keyboard: [
+    //                 [{ text: 'yes' }, { text: 'no' }]
+    //             ],
+    //             one_time_keyboard: true
+    //         }
+    //     });
+    // }
 });
 
 export default fooMiddleware;
