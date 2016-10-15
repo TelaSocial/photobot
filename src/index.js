aa3b378a9ae6c5650cc67df6a927cd1dfdf535c5
Frederico Marques <frederico.marques.22@gmail.com>
2016-10-15 20:23:55 -0300

    required fixes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import Telegraf from 'telegraf';
-const botName = process.env.BOT_NAME;
-const app = new Telegraf(process.env.BOT_TOKEN, { username: botName });
 import debugMiddleware from './middlewares/debug';
 import photoUploadMiddleware from './middlewares/photoUpload';
 import tos from './middlewares/termsOfService';
 
-console.log('token', process.env.BOT_TOKEN);
+const app = new Telegraf(
+        process.env.BOT_TOKEN,
+        { username: process.env.BOT_NAME });
 
 app.use(debugMiddleware);
 app.use(photoUploadMiddleware);
--- src/middlewares/termsOfService.js
+++ src/middlewares/termsOfService.js
@@ -1,4 +1,4 @@
-import setUp from '../../setUp';
+import setup from '../../setup';
 
 // TODO: change this to remote database
 const activeUsers = new Set();
@@ -7,23 +7,23 @@ const tos = (ctx, next) => next().then(() => {
     const { text, from } = ctx.update.message;
 
     const isStart = text && text === '/start';
-    const isTosReply = text && (text === setUp.agreeText || text === setUp.declineText);
+    const isTosReply = text && (text === setup.agreeText || text === setup.declineText);
     const hasAccepted = activeUsers.has(from.id);
 
     if (isStart && !hasAccepted) {
-        ctx.reply(setUp.askForConfirmation, { reply_markup: {
+        ctx.reply(setup.askForConfirmation, { reply_markup: {
             resize_keyboard: true,
             one_time_keyboard: true,
             keyboard: [
-                [{ text: setUp.agreeText }, { text: setUp.declineText }]]
+                [{ text: setup.agreeText }, { text: setup.declineText }]]
         }
     });
     } else if (isTosReply && !hasAccepted) {
-        if (text === setUp.agreeText) {
+        if (text === setup.agreeText) {
             activeUsers.add(from.id);
-            ctx.reply(setUp.notifyAgreed);
+            ctx.reply(setup.notifyAgreed);
         } else {
-            ctx.reply(setUp.notifyDeclined);
+            ctx.reply(setup.notifyDeclined);
         }
     }
 });
