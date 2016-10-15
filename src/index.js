7106ba2124e772f62c595f07167038e9a59b60c4
Fabricio C Zuardi <fabricio@fabricio.org>
2016-10-15 13:31:19 -0300

    instatiates telegraf with a bot username

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import Telegraf from 'telegraf';
 import fooMiddleware from './fooMiddleware';
 
-const app = new Telegraf(process.env.BOT_TOKEN);
+const app = new Telegraf(
+    process.env.BOT_TOKEN,
+    { username: process.env.BOT_USERNAME }
+);
 
 app.use(fooMiddleware);
 
