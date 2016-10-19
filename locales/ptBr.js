74ce7a75c7f384af0488fc5c67c0b988861e88a4
Fabricio C Zuardi <fabricio@fabricio.org>
2016-10-19 12:42:11 -0200

    minor license text fix

--- locales/ptBr.js
+++ locales/ptBr.js
@@ -1,5 +1,5 @@
 export default {
-    askForConfirmation: 'Do you agree to license all content submitted to this bot under a Creative Commons Attribution 4.0? If you grant the world this license to your content then it can be republished on the TV screens, on the web and anywhere else freely as long as the conditions of the license is respected. For more information about the license visit https://creativecommons.org/licenses/by/4.0/deed.pt_BR',
+    askForConfirmation: 'Do you agree to license all content submitted to this bot under a Creative Commons Attribution 4.0? If you grant the world this license to your content then it can be republished on TV screens, on the web and anywhere else freely as long as the conditions of the license is respected. For more information about the license visit https://creativecommons.org/licenses/by/4.0/deed.pt_BR',
     agreeText: 'Yes, I agree.',
     declineText: 'No, I don\'t',
     notifyAgreed: 'Thank you for agreeing with the terms. Thank you for sharing your content freely with the world. We have your permission to use your submitted content from now on. You rock!',
