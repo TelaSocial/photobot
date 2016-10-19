379021badda69dbe42a65db3768dd87e8bfc3f5c
Fabricio C Zuardi <fabricio@fabricio.org>
2016-10-19 12:37:03 -0200

    Better explanation about the content licensing.

--- locales/ptBr.js
+++ locales/ptBr.js
@@ -1,8 +1,8 @@
 export default {
-    askForConfirmation: 'Do you agree with our terms?',
-    agreeText: 'Yes, I do',
+    askForConfirmation: 'Do you agree to license all content submitted to this bot under a Creative Commons Attribution 4.0? If you grant the world this license to your content then it can be republished on the TV screens, on the web and anywhere else freely as long as the conditions of the license is respected. For more information about the license visit https://creativecommons.org/licenses/by/4.0/deed.pt_BR',
+    agreeText: 'Yes, I agree.',
     declineText: 'No, I don\'t',
-    notifyAgreed: 'Nice, you are good to go. Photos you sent from now on will be publicly licensed.',
+    notifyAgreed: 'Thank you for agreeing with the terms. Thank you for sharing your content freely with the world. We have your permission to use your submitted content from now on. You rock!',
     notifyDeclined: 'Maybe next time',
     greeting: (userName, botName) => `Welcome ${userName}. Please talk to @${botName} to have your photos posted on the screen` //eslint-disable-line
 };
